996e9b0287e1ada253006599b9f8a81721dc0d2c
Maxence Jules <142577996+Maxencejules@users.noreply.github.com>
2024-02-21 20:01:23 -0500

    Create Store.c

---
+++ Store.c
@@ -0,0 +1,20 @@
+//Store display message program
+int main() {
+    // Variable Declaration
+    int appleQuantity; 
+    double applePrice; 
+    float appleReview; 
+    int appleReviewDisplay; 
+    const char appleLocation = 'F'; 
+
+    // Initial Values Assignment
+    appleQuantity = 23; 
+    applePrice = 1.49; 
+    appleReview = 82.5; 
+
+    // Casting
+    appleReviewDisplay = (int)appleReview; 
+
+    // Output
+    printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+}
